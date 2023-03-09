c9b9fdec29f434fc7ad223ca4a9f3607ef993dc2
guinotix <al375927@uji.es>
2023-03-09 13:22:07 +0100

    03: Inertia render of the chirps route

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ChirpController extends Controller
 {
@@ -14,7 +15,9 @@ class ChirpController extends Controller
     public function index(): Response
     {
         //
-        return response('Hello world!');
+        return Inertia::render('Chirps/Index', [
+
+        ]);
     }
 
     /**
