b5270959f666d16369691226ef42aecbf9c6e7cb
Alejandro Guinot <al375927@uji.es>
2024-01-31 22:44:49 +0100

    03: routing index to helloworld response

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ChirpController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource. 
+     */
+    public function index(): Response
+    {
+        return response('Hello, World!');
+    }
 }
